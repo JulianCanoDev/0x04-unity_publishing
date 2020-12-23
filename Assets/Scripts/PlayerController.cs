7425f11f3fe16089f3b235190ee7aba044d88362
julian <juliancano.dev@gmail.com>
2020-12-22 19:24:34 -0500

    Task 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Transform Player;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-
+        Player = GetComponent <Transform> ();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey ("w")) {
-            pos.z += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w")) {
+            Player.Translate (new Vector3(0, 0, -speed) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey ("s")) {
-            pos.z -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")) {
+            Player.Translate (new Vector3(0, 0, speed) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey ("d")) {
-            pos.x += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")) {
+            Player.Translate (new Vector3(speed, 0, 0) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey ("a")) {
-            pos.x -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a")) {
+            Player.Translate (new Vector3(-speed, 0, 0) * Time.deltaTime);
         }
-
-        transform.position = pos;
     }
 }
